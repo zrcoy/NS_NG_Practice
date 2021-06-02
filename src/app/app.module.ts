1df36994e64ac1d6b01d924cd25641e8b3fc9043
zrcoy <zhangruichaotyler@gmail.com>
2021-06-02 17:57:33 -0400

    grid layout assign finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptModule } from '@nativescript/angular'
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "@nativescript/angular";
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { ItemsComponent } from './item/items.component'
-import { ItemDetailComponent } from './item/item-detail.component'
+import { AppComponent } from "./app.component";
+import { GridLayoutAssignComponent } from "./layouts/grid-layout-assign/grid-layout-assign.component";
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
-  declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
+  imports: [NativeScriptModule],
+  declarations: [AppComponent, GridLayoutAssignComponent],
   providers: [],
-  schemas: [NO_ERRORS_SCHEMA],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule {}
---
+++ src/app/layouts/grid-layout-assign/grid-layout-assign.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "ns-grid-layout-assign",
+  templateUrl: "./grid-layout-assign.component.html",
+  styleUrls: ["./grid-layout-assign.component.css"]
+})
+export class GridLayoutAssignComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
