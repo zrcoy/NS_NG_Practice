03b76aa5cd86f2efbbf20816c830cef03e39b0b0
zrcoy <zhangruichaotyler@gmail.com>
2021-06-15 20:45:14 -0400

    style the today.component and current-challenge component, add calendar grid to current challenge

--- src/app/challenge/current-challenge/current-challenge.component.ts
+++ src/app/challenge/current-challenge/current-challenge.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef } from "@angular/core";
+import { Component, OnInit, ViewContainerRef } from "@angular/core";
 import { ModalDialogService } from "@nativescript/angular";
 import { UIService } from "~/app/shared/ui/ui.service";
 import { DayModalComponent } from "../day-modal/day-modal.component";
@@ -6,17 +6,42 @@ import { DayModalComponent } from "../day-modal/day-modal.component";
 @Component({
   selector: "ns-current-challenge",
   templateUrl: "./current-challenge.component.html",
-  styleUrls: [
-    "./current-challenge.component.common.css",
-    "./current-challenge.component.css"
-  ]
+  styleUrls: ["./_current-challenge.component.common.scss"]
 })
-export class CurrentChallengeComponent {
+export class CurrentChallengeComponent implements OnInit {
+  weekDays = ["S", "M", "T", "W", "T", "F", "S"];
+  days: { dayInMonth: number; dayInWeek: number }[];
+  private _currentYear: number;
+  private _currentMonth: number;
+
   constructor(
     private modalDialog: ModalDialogService,
     private vcRef: ViewContainerRef,
     private uiService: UIService
   ) {}
+
+  ngOnInit() {
+    const daysInCurrentMonth = new Date().getDate();
+    for (let i = 1; i <= daysInCurrentMonth; i++) {
+      const date = new Date(this._currentYear, this._currentMonth, i);
+      this.days.push({
+        dayInMonth: i,
+        dayInWeek: date.getDay()
+      });
+    }
+  }
+
+  getRow(index: number, day: { dayInMonth: number; dayInWeek: number }) {
+    const startRow = 1;
+    const weekRow = Math.floor(index / 7);
+    const firstWeekdayOfThisMonth = new Date(
+      this._currentYear,
+      this._currentMonth,
+      1
+    ).getDay();
+    const possibleRow = day.dayInWeek < firstWeekdayOfThisMonth ? 1 : 0;
+    return startRow + weekRow + possibleRow;
+  }
 
   onChangeStatus() {
     this.modalDialog
--- src/app/challenge/today/today.component.ts
+++ src/app/challenge/today/today.component.ts
@@ -3,6 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "ns-today",
   templateUrl: "./today.component.html",
-  styleUrls: ["./today.component.css"]
+  styleUrls: ["./today.component.scss"]
 })
 export class TodayComponent {}
